f24d284136594a85a6f72c6bd33f24fd841552bc
Gyoungmin Kim <kimkm0419@naver.com>
2024-08-11 03:41:15 +0900

    :sparkles: feat: implement bank account entity w/ repository (#8)

---
+++ src/main/java/kr/junction/delavery/repository/BankAccountRepository.java
@@ -0,0 +1,9 @@
+package kr.junction.delavery.repository;
+
+import kr.junction.delavery.domain.BankAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
+}

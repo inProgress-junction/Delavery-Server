483a5e0b80e8c5c0657b8a9a1e99686d2ec4ad79
Gyoungmin Kim <kimkm0419@naver.com>
2024-08-10 20:57:06 +0900

    :heavy_plus_sign: chore: add initial dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,29 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // Spring
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // Swagger
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+
+    // Jasypt
+    implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.5")
+
+    // Lombok
+    implementation("org.projectlombok:lombok:1.18.32")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
+
+    // MySQL
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    // JPA
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // Flyway
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-mysql")
 }
 
 tasks.withType<Test> {

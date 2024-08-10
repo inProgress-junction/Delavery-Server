57b2bb0f6e8cb80d76bebd49be52dac86945ae25
Gyoungmin Kim <kimkm0419@naver.com>
2024-08-10 20:31:14 +0900

    :tada: Initial commit

---
+++ src/main/java/kr/junction/delavery/DelaveryApplication.java
@@ -0,0 +1,13 @@
+package kr.junction.delavery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelaveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelaveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/junction/delavery/DelaveryApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.junction.delavery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelaveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

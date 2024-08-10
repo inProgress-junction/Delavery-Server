b220ee7d4f59e0a9e1f7cbd590a796555bfb46c8
Gyoungmin Kim <kimkm0419@naver.com>
2024-08-11 08:23:59 +0900

    :bug: fix: fix api interface (#13)

--- src/main/java/kr/junction/delavery/api/ChallengeApi.java
+++ src/main/java/kr/junction/delavery/api/ChallengeApi.java
@@ -1,6 +1,7 @@
 package kr.junction.delavery.api;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.junction.delavery.common.annotation.RequestUserId;
 import kr.junction.delavery.controller.dto.response.ChallengeResponse;
@@ -16,18 +17,18 @@ public interface ChallengeApi {
     @Operation(summary = "챌린지 백분위 조회")
     @GetMapping("/percentile")
     PercentileResponse getChallengePercentile(
-            @RequestUserId String memberId
+            @RequestUserId @Parameter(hidden = true) String memberId
     );
 
     @Operation(summary = "챌린지 언락 시도 현황 변경")
     @PatchMapping("/unlock-trial-count")
     ChallengeResponse updateUnlockTrialCount(
-            @RequestUserId String memberId
+            @RequestUserId @Parameter(hidden = true) String memberId
     );
 
     @Operation(summary = "챌린지 언락 현황 변경")
     @PatchMapping("/unlock-done-count")
     ChallengeResponse updateUnlockDoneCount(
-            @RequestUserId String memberId
+            @RequestUserId @Parameter(hidden = true) String memberId
     );
 }
